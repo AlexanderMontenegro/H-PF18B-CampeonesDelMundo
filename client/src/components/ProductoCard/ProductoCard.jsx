542d43fa356f00936f8cb0df09b98080e5127271
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-06-27 19:39:42 -0300

    Modificacion: ProductCard+ProductList+css

--- client/src/components/ProductoCard/ProductoCard.jsx
+++ client/src/components/ProductoCard/ProductoCard.jsx
@@ -7,14 +7,21 @@ const ProductoCard = ({ producto }) => {
 
   return (
     <div className="card__container">
-      <Link to={`/product/${producto.id}`} className="card__link">Info</Link>
-      <img src={producto.imagen} alt={`${producto.tipo} ${producto.marca}`} className="card__image" />
+      {/*<img src={producto.imagen} alt={`${producto.tipo} ${producto.marca}`} className="card__image" />*/}
+      <Link to={`/product/${producto.id}`} className="card__link"> <img src={producto.imagen} alt={`${producto.tipo} ${producto.marca}`} className="card__image" /></Link>
+
+
       <div className="card__details">
         <h3 className="card__title">{`${producto.tipo} ${producto.marca}`}</h3>
         <p className="card__name">Precio: ${producto.precio}</p>
-        <p className="card__name">Talles: {producto.talles.join(', ')}</p>
+       {/*  <p className="card__name">Talles: {producto.talles.join(', ')}</p>
+        
+        
         <p className="card__name">Categoría: {producto.categoria}</p>
         <p className="card__name">País: {producto.pais}</p>
+
+        <Link to={`/product/${producto.id}`} className="card__link"><img className='ico_detalle' src="../../../public/iconos/informacion.png" alt="Detalle"  /></Link>
+        */}
         <div className="card__buttons">
           <button className="card__cart-button">Agregar al Carrito</button>
           <button className="card__fav-button">Favoritos</button>
