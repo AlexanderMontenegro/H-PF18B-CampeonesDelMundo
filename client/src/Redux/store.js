933ce374b06e53671aefe7495cf43a3bba334701
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-06-27 01:21:32 -0300

    error:con ultimo pull

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -1,5 +1,5 @@
 import {createStore, applyMiddleware, compose} from 'redux'
-import {thunk} from 'redux-thunk'
+import {thunk} from 'redux-thunk';
 import rootReducer from './reducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
