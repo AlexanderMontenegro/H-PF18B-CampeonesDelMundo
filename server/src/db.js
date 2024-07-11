9af9a0c09d768fcfd381e5c395579b5c301c265c
joshuaavalo <avalojoshua83@gmail.com>
2024-07-11 15:56:53 -0300

    actualizacion para deploy 2.0

--- server/src/db.js
+++ server/src/db.js
@@ -4,19 +4,21 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+const {  DATABASE_UR } = process.env;
 
-
-const sequelize = new Sequelize(process.env.DB_URL, {
-   dialect: 'postgres',
-   dialectOptions: {
-     ssl: {
-       require: true,
-       rejectUnauthorized: false,
+const sequelize = new Sequelize(
+   `${DATABASE_UR}`,
+   {
+     logging: false, // set to console.log to see the raw SQL queries
+     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+     dialectOptions: {
+       ssl: {
+         require: true,
+         rejectUnauthorized: false, // Puedes establecer esto en true si tienes un certificado SSL válido
+       },
      },
-   },
- });
-
+   }
+ );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
