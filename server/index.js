55c97925b2b4f017a17267bfc08bf2e1b5ac628c
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-07-12 19:14:17 -0300

    A2

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const PORT = 3001;
+const PORT = process.env.PORT||400;
 
 conn.sync({ force: true }).then(() => {
 server.listen(PORT, () => {
