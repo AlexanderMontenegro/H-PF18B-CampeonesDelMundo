8f7176b4720a571dbf366b9fb7f0404fd65a2239
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-07-13 23:22:58 -0300

    Cracion: Socket chat + css

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/*
 const axios = require("axios");
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
@@ -8,3 +9,44 @@ server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 })
 }).catch(error => console.error(error))
+*/
+
+const axios = require("axios");
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const server = require("./src/server");
+const { conn } = require('./src/db.js');
+const PORT = process.env.PORT || 4000; 
+
+
+const app = express();
+const httpServer = http.createServer(app);
+
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*", 
+    methods: ["GET", "POST"]
+  }
+});
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('message', (msg) => {
+    io.emit('message', msg); 
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+
+conn.sync({ force: true }).then(() => {
+  httpServer.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
+}).catch(error => console.error(error));
