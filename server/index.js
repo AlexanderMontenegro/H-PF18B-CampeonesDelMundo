3d7cf10e8e4da8e1094b5e03b15e1c47e2066a13
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-07-15 01:10:17 -0300

    Creacion: Conteo de usuarios en linea Socket

--- server/index.js
+++ server/index.js
@@ -1,15 +1,3 @@
-/*
-const axios = require("axios");
-const server = require("./src/server");
-const { conn } = require('./src/db.js');
-const PORT = process.env.PORT||400;
-
-conn.sync({ force: true }).then(() => {
-server.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
-}).catch(error => console.error(error))
-*/
 
 const axios = require("axios");
 const express = require("express");
