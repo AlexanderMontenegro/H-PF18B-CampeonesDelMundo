2991c049e556d2a6a2831b4ae24e51f37d1c560f
Seven00321 <richrdpere123@gmail.com>
2024-07-15 23:14:39 -0500

    MODIFICAICON: en backend.RMPCH

--- server/src/server.js
+++ server/src/server.js
@@ -3,13 +3,15 @@ const router = require("./routes");
 const morgan = require("morgan");
 const cors = require("cors");
 
+
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(cors());
 server.use('/api', router);
-server.use(router)
+server.use(router);
+ç
 
 
 
