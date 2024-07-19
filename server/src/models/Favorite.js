64190e17466543fc9d6ac6951d2bd429e18d389b
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-07-19 19:39:53 -0300

    Actual 19/7-19.39

--- server/src/models/Favorite.js
+++ server/src/models/Favorite.js
@@ -1,4 +1,3 @@
-/*
 const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
@@ -21,45 +20,6 @@ module.exports = (sequelize) => {
     tableName: 'favorites',
     timestamps: false,
   });
-
-  return Favorite;
-};
-
-*/
-
-const { DataTypes } = require('sequelize');
-
-module.exports = (sequelize) => {
-  const Favorite = sequelize.define('Favorite', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    productos_id: {
-      type: DataTypes.UUID,
-      allowNull: false,
-    },
-  }, {
-    tableName: 'favorites',
-    timestamps: false,
-  });
-
-  Favorite.associate = (models) => {
-    Favorite.belongsTo(models.User, {
-      foreignKey: 'user_id',
-      as: 'user',
-    });
-    Favorite.belongsTo(models.Productos, {
-      foreignKey: 'productos_id',
-      as: 'producto',
-    });
-  };
 
   return Favorite;
 };
