45571983cac960620a41d9a165bcb5184b789518
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-07-19 01:09:06 -0300

    Actualizacion : 19/07-1.09

--- server/src/routes/favoriteRoutes.js
+++ server/src/routes/favoriteRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { addFavorite, getUserFavorites, removeFavorite } = require('../controllers/favoriteController');
+const { addFavorite, getUserFavorites, removeFavorite } = require('../controllers/favoritecontroller');
 
-
+// Ruta para agregar un favorito
 router.post('/favorites', addFavorite);
 
-
+// Ruta para obtener favoritos de un usuario
 router.get('/favorites/:userId', getUserFavorites);
 
-
+// Ruta para eliminar un favorito
 router.delete('/favorites/:favoriteId', removeFavorite);
 
 module.exports = router;
