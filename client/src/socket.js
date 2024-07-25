31d5394de4e296578bd9be9109d37da6113b0ef7
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-07-25 13:21:18 -0300

    Actualizacion 15/7-13.21

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,18 @@
 import { io } from 'socket.io-client';
 
+
+/*Local */
+/*
 const socket = io('http://localhost:3001'); 
+*/
+
+
+
+/*Deply */
+
+const socket = io('https://h-pf18b-campeonesdelmundo-b.onrender.com'); 
+
+
+
 
 export default socket;
