94d0fbbff9e689de998ed24eaecaeb7d5af3fa87
Alexander Montenegro <alexandermontenegro0691@gmail.com>
2024-07-24 22:20:48 -0300

    Modificacion 24/07-22.20

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -16,8 +16,8 @@ import { Provider } from 'react-redux';
 import store from './Redux/store.js';
 import axios from 'axios'
 
-//axios.defaults.baseURL="https://h-pf18b-campeonesdelmundo-ssxu.onrender.com"
-axios.defaults.baseURL="http://localhost:3001/"
+axios.defaults.baseURL="https://h-pf18b-campeonesdelmundo-b.onrender.com"
+//axios.defaults.baseURL="http://localhost:3001/"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
